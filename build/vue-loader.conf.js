39e972d8ee11fbf7432b3e9249eaa2058410bfcb
zhangjunTracy <13552960783@163.com>
2018-08-19 18:51:36 +0800

    改成不需要单独引入css样式

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,7 +1,9 @@
 'use strict'
 const utils = require('./utils')
 const config = require('../config')
-const isProduction = process.env.NODE_ENV === 'production'
+// 不把style单独抽取出来，样式少，多的话可以抽取出啦，但是得单独引用css文件
+// const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = false
 const sourceMapEnabled = isProduction
   ? config.build.productionSourceMap
   : config.dev.cssSourceMap
